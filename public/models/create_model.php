2bfeb8489145644f8f6b71571e2c75e0c36f36ff
boyko1989 <hunwiss89@gmail.com>
2020-12-07 22:27:25 +0400

    Добавляются статьи в базу через форму на сайте

--- public/models/create_model.php
+++ public/models/create_model.php
@@ -1,6 +1,7 @@
 <?php //defined("CATALOG") or die("Access denied");
 function insert_content($theme_title, $theme_parent, $content_articles, $name_articles, $article_alias) {
-    global $connection;
+   // session_start();
+		global $connection;
     $query_check = "SELECT `title`, `id` FROM `theme` WHERE `title`='$theme_title'";
     $res_check = mysqli_query($connection, $query_check);
     $rows = mysqli_num_rows($res_check);
@@ -12,9 +13,14 @@ function insert_content($theme_title, $theme_parent, $content_articles, $name_ar
     $res_theme_id = mysqli_query($connection, $query_theme_id);
     $res_theme_id = mysqli_fetch_all($res_theme_id, MYSQLI_ASSOC);    
     $theme_id = $res_theme_id[0]['id'];
+		// echo $theme_id;
+		 //$_SESSION = ["theme_id" => $theme_id];
         
-    $query_txt = "INSERT INTO `articles`(`id`, `parent`, `title`, `content`, `alias`, `image`) VALUES (NULL, '$theme_id', '$name_articles', '$content_articles', '$article_alias', NULL)";
+    $query_txt = "INSERT INTO `articles`(`id`, `parent`, `title`, `content`, `alias`, `image`) VALUES (NULL, '$theme_id', '$name_articles', '$content_articles', '$article_alias', 'thumb');";
+		//echo $query_txt;
     $res_txt = mysqli_query($connection, $query_txt);
+		
+//echo $theme_id;
 }
 function get_option_theme() {
     global $connection;
