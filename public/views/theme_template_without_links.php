176c702904b9f8b9db8ba62b5256e31a5809186e
boyko1989 <hunwiss89@gmail.com>
2020-12-12 10:54:30 +0400

    Маленькое исправление шаблона без ссылок

--- public/views/theme_template_without_links.php
+++ public/views/theme_template_without_links.php
@@ -2,7 +2,7 @@
 	<a href="<?=PATH?>themeditor/<?=$theme['id']?>"><?=$theme['title']?></a>
 	<?php if( isset($theme['childs']) && $theme['childs'] ): ?>
 	<ul>
-		<?php echo theme_to_string($theme['childs']); ?>
+		<?php echo theme_to_string_without_links($theme['childs']);?>
 	</ul>
 	<?php endif; ?>
 </li>
