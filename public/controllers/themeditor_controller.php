78224341d8eb10e76fdc25f9f35292e1666ac48b
boyko1989 <hunwiss89@gmail.com>
2020-12-12 10:35:17 +0400

    Строим дерево тем в редакторе тем

--- public/controllers/themeditor_controller.php
+++ public/controllers/themeditor_controller.php
@@ -4,7 +4,7 @@ require_once('main_controller.php');
 require_once('models/themeditor_model.php');
 $theme = get_theme();
 $theme_tree = map_tree($theme);
-$theme_menu = theme_to_string_without_links($theme_tree);
+$theme_menu_without_links = theme_to_string_without_links($theme_tree);
 
 require_once('views/themeditor.php');
 
