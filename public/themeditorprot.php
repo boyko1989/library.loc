c4b07fc2622df5d470d9c23cc3bcf4b1325d13f2
boyko1989 <hunwiss89@gmail.com>
2020-12-15 20:07:17 +0400

    Сделал вариант списка статей из форм

--- public/themeditorprot.php
+++ public/themeditorprot.php
@@ -1,3 +1,9 @@
+<?php 
+
+$tab = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+$dtab = $tab.$tab;
+
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -39,70 +45,163 @@
         </li>	</ul>
         </li>
 
-    <h2>Список потомков</h2>
-				<form></form>
-    <h2>Список статей</h2>
-    <table border="1">
+<h2>Список потомков</h2>
+<h2>Список статей - вариант с формами</h2>
+    
+    </table>
+    <form action="#" method="POST">
+        <input type="text" value="Переохлаждённый туман" class="title-table-input"> 
+        <select>
+            <option value="ID темы">Общая</option>
+            <option value="ID темы"><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+            <option value="ID темы"><?=$dtab?>Бутерброды</option>
+            <option value="ID темы"><?=$tab?>КМ468</option>
+            <option value="ID темы"><?=$tab?>Проверка</option>
+            <option value="ID темы"><?=$tab?>Этот сайт</option>
+            <option value="ID темы"><?=$dtab?>Строение</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+        </select>
+        <a href="#" class="delete-bottom">Удалить</a>
+        <input type="submit" class="save-bottom" value="Сохранить">
+    </form>
+
+    <form action="#" method="POST">
+        <input type="text" value="Вместо колбасы на бутерброды! Очень вкусная холодная закуска из куриного филе" class="title-table-input"> 
+        <select>
+            <option value="ID темы">Общая</option>
+            <option value="ID темы"><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+            <option value="ID темы"><?=$dtab?>Бутерброды</option>
+            <option value="ID темы"><?=$tab?>КМ468</option>
+            <option value="ID темы"><?=$tab?>Проверка</option>
+            <option value="ID темы"><?=$tab?>Этот сайт</option>
+            <option value="ID темы"><?=$dtab?>Строение</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+        </select>
+        <a href="#" class="delete-bottom">Удалить</a>
+        <input type="submit" class="save-bottom" value="Сохранить">
+    </form>
+
+    <form action="#" method="POST">
+        <input type="text" value="К неделе от 14.12.2020" class="title-table-input"> 
+        <select>
+            <option value="ID темы">Общая</option>
+            <option value="ID темы"><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+            <option value="ID темы"><?=$dtab?>Бутерброды</option>
+            <option value="ID темы"><?=$tab?>КМ468</option>
+            <option value="ID темы"><?=$tab?>Проверка</option>
+            <option value="ID темы"><?=$tab?>Этот сайт</option>
+            <option value="ID темы"><?=$dtab?>Строение</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+        </select>
+        <a href="#" class="delete-bottom">Удалить</a>
+        <input type="submit" class="save-bottom" value="Сохранить">
+    </form>
+
+    <form action="#" method="POST">
+        <input type="text" value="Проблемы, кажется, с буферизацией на сервере..." class="title-table-input"> 
+        <select>
+            <option value="ID темы">Общая</option>
+            <option value="ID темы"><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+            <option value="ID темы"><?=$dtab?>Бутерброды</option>
+            <option value="ID темы"><?=$tab?>КМ468</option>
+            <option value="ID темы"><?=$tab?>Проверка</option>
+            <option value="ID темы"><?=$tab?>Этот сайт</option>
+            <option value="ID темы"><?=$dtab?>Строение</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+        </select>
+        <a href="#" class="delete-bottom">Удалить</a>
+        <input type="submit" class="save-bottom" value="Сохранить">
+    </form>
+
+    <form action="#" method="POST">
+        <input type="text" value="Проверка" class="title-table-input"> 
+        <select>
+            <option value="ID темы">Общая</option>
+            <option value="ID темы"><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+            <option value="ID темы"><?=$dtab?>Бутерброды</option>
+            <option value="ID темы"><?=$tab?>КМ468</option>
+            <option value="ID темы"><?=$tab?>Проверка</option>
+            <option value="ID темы"><?=$tab?>Этот сайт</option>
+            <option value="ID темы"><?=$dtab?>Строение</option>
+            <option value="ID темы"><?=$tab?>Кулинария</option>
+        </select>
+        <a href="#" class="delete-bottom">Удалить</a>
+        <input type="submit" class="save-bottom" value="Сохранить">
+    </form>
+
+
+<h2>Список статей - вариант с таблицей</h2>
+    <table>
         <tr>
             <th class="col1">Название</th>
             <th class="col2">Родитель</th>
             <th class="col3">Удаление</th>
             <th class="col4">Сохранить</th>
         </tr>
+
         <tr>
             <td class="title-table">
-									<form>
-											<input type="text" value="Переохлаждённый туман" width="320"> 
-									</form>
-							</td>
-            <td class="parent-table">
+                <form>
+                    <input type="text" value="Переохлаждённый туман"> 
+                </form>
+            </td>
+            <td class="parent-table" name="parent_theme_id"> <!-- Не знаю, куда лучше записывать name="" - сюда или в селект... -->
                 <select>
-                    <option>Общая</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Явления природы, влияющие на тактику дворнических работ</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Бутерброды</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;КМ468</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Проверка</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Этот сайт</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Строение</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
+                    <option value="ID темы">Общая</option>
+                    <option value="ID темы"><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+                    <option value="ID темы"><?=$tab?>Кулинария</option>
+                    <option value="ID темы"><?=$dtab?>Бутерброды</option>
+                    <option value="ID темы"><?=$tab?>КМ468</option>
+                    <option value="ID темы"><?=$tab?>Проверка</option>
+                    <option value="ID темы"><?=$tab?>Этот сайт</option>
+                    <option value="ID темы"><?=$dtab?>Строение</option>
+                    <option value="ID темы"><?=$tab?>Кулинария</option>
                 </select>
             </td>
-            <td class="delete-bottom-td"><a href="#" class="delete-bottom">Удалить</a></td>
-            <td class="save-bottom-td"><a href="#" class="save-bottom">Сохранить</a></td>
+            <td class="delete-bottom-td"><a href="#" class="delete-bottom">Удалить</a></td><!-- Отправляемся на скрипт удаления -->
+            <td class="save-bottom-td"><a href="#" class="save-bottom">Сохранить</a></td><!-- Отправляемся на скрипт update -->
         </tr>
 
         <tr>
-            <td>Вместо колбасы на бутерброды! Очень вкусная холодная закуска из куриного филе</td>
-            <td class="parent-table">
-                <select>
-                    <option>Общая</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Явления природы, влияющие на тактику дворнических работ</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Бутерброды</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;КМ468</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Проверка</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Этот сайт</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Строение</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
-                </select>
-            </td>
-            <td class="delete-bottom-td"><a href="#" class="delete-bottom">Удалить</a></td>
-            <td class="save-bottom-td"><a href="#" class="save-bottom">Сохранить</a></td>
+            <form>
+                <td class="title-table"><input type="text" value="Вместо колбасы на бутерброды! Очень вкусная холодная закуска из куриного филе"> </td>
+                <td class="parent-table">
+                
+                    <select>
+                        <option>Общая</option>
+                        <option><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+                        <option><?=$tab?>Кулинария</option>
+                        <option><?=$dtab?>Бутерброды</option>
+                        <option><?=$tab?>КМ468</option>
+                        <option><?=$tab?>Проверка</option>
+                        <option><?=$tab?>Этот сайт</option>
+                        <option><?=$dtab?>Строение</option>
+                        <option><?=$tab?>Кулинария</option>
+                    </select>
+                </td>
+                <td class="delete-bottom-td"><a href="#" class="delete-bottom">Удалить</a></td>
+                <td class="save-bottom-td"><a href="#" class="save-bottom">Сохранить</a></td>
+            </form>
         </tr>
+
         <tr>
             <td>К неделе от 14.12.2020</td>
             <td class="parent-table">
-                <select>
+            <select>
                     <option>Общая</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Явления природы, влияющие на тактику дворнических работ</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Бутерброды</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;КМ468</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Проверка</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Этот сайт</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Строение</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
+                    <option><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+                    <option><?=$tab?>Кулинария</option>
+                    <option><?=$dtab?>Бутерброды</option>
+                    <option><?=$tab?>КМ468</option>
+                    <option><?=$tab?>Проверка</option>
+                    <option><?=$tab?>Этот сайт</option>
+                    <option><?=$dtab?>Строение</option>
+                    <option><?=$tab?>Кулинария</option>
                 </select>
             </td>
             <td class="delete-bottom-td"><a href="#" class="delete-bottom">Удалить</a></td>
@@ -111,16 +210,16 @@
         <tr>
             <td>Проблемы, кажется, с буферизацией на сервере...</td>
             <td class="parent-table">
-                <select>
+            <select>
                     <option>Общая</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Явления природы, влияющие на тактику дворнических работ</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Бутерброды</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;КМ468</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Проверка</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Этот сайт</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Строение</option>
-                    <option>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Кулинария</option>
+                    <option><?=$tab?>Явления природы, влияющие на тактику дворнических работ</option>
+                    <option><?=$tab?>Кулинария</option>
+                    <option><?=$dtab?>Бутерброды</option>
+                    <option><?=$tab?>КМ468</option>
+                    <option><?=$tab?>Проверка</option>
+                    <option><?=$tab?>Этот сайт</option>
+                    <option><?=$dtab?>Строение</option>
+                    <option><?=$tab?>Кулинария</option>
                 </select>
             </td>
             <td class="delete-bottom-td"><a href="#" class="delete-bottom">Удалить</a></td>
@@ -131,17 +230,21 @@
         body {
             background-color: rgb(247, 243, 243);
         }
-        TABLE {
+        form {
+            padding: 0;
+            margin: 0;
+        }
+        table.fixed {
             border: solid 1px black;
             border-collapse: collapse;
             table-layout: fixed; /* Фиксированная ширина ячеек */
             width: 100%; /* Ширина таблицы */
         }
         .col1 { 
-            min-width: 350px; 
+            width: 340px; 
         }
         .col2 {
-            min-width: 160px; 
+            width: 300px;
         }
         .col3 { 
             width: 80px; 
@@ -149,7 +252,48 @@
         .col4 { 
             width: 80px; 
         }
-        
+
+        .title-table-input {
+            font-family: Georgia, serif;
+            font-size: 16px;
+            width: 400px;
+            padding: 15px;
+            background-color: rgb(147, 224, 213);
+            border: none;
+            outline: none;
+        }
+        select {
+            font-family: Georgia, serif;
+            font-size: 16px;
+            width: 300px;
+            padding: 14px;
+            border: none;
+            outline: none;
+            background-color: rgb(147, 224, 213);
+            color: black; 
+        }    
+        .delete-bottom {
+            text-decoration: none;
+            color: white;
+            background-color: rgb(252, 104, 104);
+            padding: 15px;
+            border: black solid 1px;
+        }
+        .delete-bottom-td {
+           
+            text-align: center;
+        }
+        .save-bottom {
+            text-decoration: none;
+            color: white;
+            background-color: rgb(104, 163, 252);
+            padding: 15px;            
+            border: black solid 1px;
+        }
+        .save-bottom-td {
+            /* padding: 0;  */
+            text-align: center;
+        }    
         th {
             background: rgb(106, 106, 206); /* Цвет фона ячеек */
             color: white; /* Цвет текста */
@@ -159,69 +303,44 @@
         td {
             padding: 10px;
         }
-				.title-table {
-						padding: 0;
-				}
-					.title-table > form > input {
-							font-family: Georgia, serif;
-							font-size: 16px;
-							margin: 10px;
-							padding 15px;
-							background-color: rgb(147, 224, 213);
-							border: none;
-							outline: none;
-					}
+        .title-table {
+                padding: 0;
+        }
+        .title-table > form > input {
+            font-family: Georgia, serif;
+            font-size: 16px;
+            width: 280px;
+            padding: 15px;
+            background-color: rgb(147, 224, 213);
+            border: none;
+            outline: none;
+        }
+        .title-table > input {
+            font-family: Georgia, serif;
+            font-size: 16px;
+            width: 280px;
+            padding: 15px;
+            background-color: rgb(147, 224, 213);
+            border: none;
+            outline: none;
+        }
         tr {
             background: rgb(147, 224, 213); /* Цвет фона таблицы */
             color: black;            
-        } 
-        select {
-            width: 160px;
-            padding: 10px;
-            border: none;
-            position: relative;
-            outline: none;
-            background-color: rgb(147, 224, 213);
-            color: black; 
-        }
-               
+        }     
         .parent-table {
             padding: 0; 
             text-align: right;
         }
-        .delete-bottom {
-            text-decoration: none;
-            color: white;
-            background-color: rgb(252, 104, 104);
-            padding: 8px;
-            border-radius: 5px;
-            border: black solid 1px;
-        }
-        .delete-bottom-td {
-            padding: 0; 
-            text-align: center;
-        }
-        .save-bottom {
-            text-decoration: none;
-            color: white;
-            background-color: rgb(104, 163, 252);
-            padding: 8px;
-            border-radius: 5px;
-            border: black solid 1px;
-        }
-        .save-bottom-td {
-            padding: 0; 
-            text-align: center;
-        }
+        
         @media all and (max-width: 720px) {
-            html {
-                /*transform: rotate(-90deg);*/
-									transform: rotate(90deg); 
-										transform-origin: bottom left; 
-										position: absolute; 
-										top: -100vw; 
-										left: 0;
-            			}
+            html {                
+                    transform: rotate(90deg); 
+                    transform-origin: bottom left; 
+                    position: absolute; 
+                    top: -100vw; 
+                    left: 0;
+                }
             .col1 { 
                 width: 285px; 
             }
