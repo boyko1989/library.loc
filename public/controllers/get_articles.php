f09926796b89c4c99842599bc600297257867278
boyko1989 <hunwiss89@gmail.com>
2020-12-22 20:47:29 +0400

    Изменения для предотвращения 500 ошибки сервера - окончательные: По невнимательности не сохранил один файлик :-)

--- public/controllers/get_articles.php
+++ public/controllers/get_articles.php
@@ -2,7 +2,7 @@
     $theme = $_POST;
     $theme_id = $_POST['id']; 
     $theme_title = $_POST['title']; 
-    $res_articles = get_articles($theme_id);
+    $res_articles = get_articles_ed($theme_id);
     if (!empty($res_articles)) $ht_code = '<h3>Список статей темы <b>'.$theme_title.'</b>:</h3>'.articles_in_form($res_articles); 
     $_SESSION['form_articles'] = $ht_code;
     header("Location: ".PATH."themeditor/");
