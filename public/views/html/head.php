bc64c35bd3af5d4c3c0ced7ea9c3d75350495197
boyko1989 <hunwiss89@gmail.com>
2021-01-08 00:00:29 +0400

    Пробую оптимизировать сайт

--- public/views/html/head.php
+++ public/views/html/head.php
@@ -28,16 +28,15 @@
 	<!-- <link rel="stylesheet" href="<?=PATH?>public/views/css/style.css"> -->
 	<link rel="stylesheet" href="<?=PATH?>public/views/css/index.min.css">
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> 
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
+	<!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>  -->
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.3/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/23.1.0/decoupled-document/ckeditor.js"></script>
 
-    <script src="<?=PATH?>public/views/js/bootstrap.min.js"></script>
-    <script src="<?=PATH?>public/views/js/summernote.js"></script>
-	<script src="<?=PATH?>public/views/js/lang/summernote-ru-RU.js"></script>
+   <script src="<?=PATH?>public/views/js/bootstrap.min.js"></script>
+   <!-- <script src="<?=PATH?>public/views/js/summernote.js"></script> -->
+	<!-- <script src="<?=PATH?>public/views/js/lang/summernote-ru-RU.js"></script> -->
 	<script src="<?=PATH?>public/views/js/script.js"></script>
 
     <title>Библиотека</title>
